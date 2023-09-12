4ca5316c54ff31476916cf7fbe17af98377e8963
lawankar.anuja08@gmail.com <lawankar.anuja08@gmail.com>
2023-09-12 17:34:19 -0400

    Add code for script

---
+++ Variables/script.js
@@ -0,0 +1,12 @@
+// Write Your JavaScript Code Here
+var personName = "Anuja";
+console.log("My name is" + personName);
+
+var NumOfPets = "3";
+
+console.log("I have pets" + NumOfPets);
+
+var FunFact = " I am a cool girl";
+
+console.log("FunFact is " + FunFact);
+
